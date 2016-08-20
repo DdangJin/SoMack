e4132297857fb0ec59c2c72140343f2749024ea5
Dong-jin Hong <Dong-jin Hong>
2016-08-20 20:34:54 +0900

    First Commit

---
+++ src/test/java/com/SoMack/SoMackApplicationTests.java
@@ -0,0 +1,16 @@
+package com.SoMack;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SoMackApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
